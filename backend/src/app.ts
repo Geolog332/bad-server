eb64f9ee7d3f1d6ccb3e87e03d2bdc4ec765d37a
Anton Mataev <Geolog332@yahoo.com>
2025-05-28 22:43:21 +0500

    fix: middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,7 @@ import express, { json, urlencoded } from 'express'
 import mongoose from 'mongoose'
 import path from 'path'
 import rateLimit from 'express-rate-limit'
+import mongoSanitize from 'express-mongo-sanitize'
 import { DB_ADDRESS } from './config'
 import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
@@ -24,13 +25,13 @@ app.use(cookieParser())
 
 // app.use(cors())
 app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }))
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(serveStatic(path.join(__dirname, 'public')))
 
 app.use(urlencoded({ extended: true }))
 app.use(json())
-
+app.use(mongoSanitize())
 app.options('*', cors())
 app.use(routes)
 app.use(errors())
